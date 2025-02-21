26bb2a1f7beef0a9c635a0928c9a33fbae8775c0
Axel <qianwangfan@ultraman.local>
2025-02-21 12:26:12 -0800

    design and construct poll

--- anchor/programs/votingdapp/src/lib.rs
+++ anchor/programs/votingdapp/src/lib.rs
@@ -1,70 +1,31 @@
 #![allow(clippy::result_large_err)]
 
 use anchor_lang::prelude::*;
+use borsh::de;
 
 declare_id!("coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF");
 
 #[program]
 pub mod votingdapp {
     use super::*;
-
-  pub fn close(_ctx: Context<CloseVotingdapp>) -> Result<()> {
-    Ok(())
-  }
-
-  pub fn decrement(ctx: Context<Update>) -> Result<()> {
-    ctx.accounts.votingdapp.count = ctx.accounts.votingdapp.count.checked_sub(1).unwrap();
-    Ok(())
-  }
-
-  pub fn increment(ctx: Context<Update>) -> Result<()> {
-    ctx.accounts.votingdapp.count = ctx.accounts.votingdapp.count.checked_add(1).unwrap();
-    Ok(())
-  }
-
-  pub fn initialize(_ctx: Context<InitializeVotingdapp>) -> Result<()> {
-    Ok(())
-  }
-
-  pub fn set(ctx: Context<Update>, value: u8) -> Result<()> {
-    ctx.accounts.votingdapp.count = value.clone();
-    Ok(())
-  }
+    pub fn initiliaze_poll(ctx: Context<InitializePoll>, poll_id: u64) -> Result<()> {
+      Ok(())
+    }
 }
 
 #[derive(Accounts)]
-pub struct InitializeVotingdapp<'info> {
-  #[account(mut)]
-  pub payer: Signer<'info>,
-
-  #[account(
-  init,
-  space = 8 + Votingdapp::INIT_SPACE,
-  payer = payer
-  )]
-  pub votingdapp: Account<'info, Votingdapp>,
-  pub system_program: Program<'info, System>,
-}
-#[derive(Accounts)]
-pub struct CloseVotingdapp<'info> {
-  #[account(mut)]
-  pub payer: Signer<'info>,
-
-  #[account(
-  mut,
-  close = payer, // close account and return lamports to payer
-  )]
-  pub votingdapp: Account<'info, Votingdapp>,
-}
-
-#[derive(Accounts)]
-pub struct Update<'info> {
-  #[account(mut)]
-  pub votingdapp: Account<'info, Votingdapp>,
+pub struct InitializePoll<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    #[account(mut)]
+    pub poll: Account<'info, Poll>,
 }
 
 #[account]
-#[derive(InitSpace)]
-pub struct Votingdapp {
-  count: u8,
+pub struct Poll {
+  pub poll_id: u64,
+  pub description: String,
+  pub poll_start: u64,
+  pub poll_end: u64,
+  pub candidates_amount: u64,
 }
