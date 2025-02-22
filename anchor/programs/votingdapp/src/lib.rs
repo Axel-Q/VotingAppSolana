f917739ca33366d4602769a5d5435e39191ee54e
Axel <qianwangfan@ultraman.local>
2025-02-21 20:50:26 -0800

    test poll function

--- anchor/programs/votingdapp/src/lib.rs
+++ anchor/programs/votingdapp/src/lib.rs
@@ -1,29 +1,52 @@
 #![allow(clippy::result_large_err)]
 
 use anchor_lang::prelude::*;
-use borsh::de;
 
 declare_id!("coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF");
 
 #[program]
 pub mod votingdapp {
+    use std::fmt::DebugSet;
+
     use super::*;
-    pub fn initiliaze_poll(ctx: Context<InitializePoll>, poll_id: u64) -> Result<()> {
+    pub fn initialize_poll(
+      ctx: Context<InitializePoll>, 
+      poll_id: u64,
+      poll_descrption: String,
+      poll_start: u64,
+      poll_end: u64,
+    ) -> Result<()> {
+      let poll = &mut ctx.accounts.poll;
+      poll.poll_id = poll_id;
+      poll.description = poll_descrption;
+      poll.poll_start = poll_start;
+      poll.poll_end = poll_end;
+      poll.candidates_amount = 0;
       Ok(())
     }
 }
 
 #[derive(Accounts)]
+#[instruction(poll_id: u64)]
 pub struct InitializePoll<'info> {
     #[account(mut)]
     pub signer: Signer<'info>,
-    #[account(mut)]
+    #[account(
+      init,
+      payer = signer, 
+      space = 8+ Poll::INIT_SPACE,
+      seeds = [poll_id.to_le_bytes().as_ref()],
+      bump
+      )]
     pub poll: Account<'info, Poll>,
+    pub system_program: Program<'info, System>,
 }
 
 #[account]
+#[derive(InitSpace)]
 pub struct Poll {
   pub poll_id: u64,
+  #[max_len(32)]
   pub description: String,
   pub poll_start: u64,
   pub poll_end: u64,
